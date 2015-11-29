fbcea56ffe8d550e82d926bb9f2cbad97f85e375
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2015-11-29 16:02:37 +0100

    Basic redux setup fro react with a sample action/reducer etc.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import AppComponent from './component/container/appComponent';
+import { Provider } from 'react-redux'
+import * as redux from 'redux';
+import reducers from './reducers';
+
+var store = redux.createStore(reducers);
 
 ReactDom.render(
-    <AppComponent />,
+    <Provider store={store}>
+        <AppComponent />
+    </Provider>,
     document.getElementById('js-app-container')
 );
